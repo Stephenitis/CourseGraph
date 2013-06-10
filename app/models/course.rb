bee1f078703b05daafe61353d9db4a8c7c26437b
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-06-10 11:50:35 -0700

    Removed home controller and views to be moved

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,15 +3,16 @@ class Course < ActiveRecord::Base
   has_many :requisites, :through => :relationships
   has_many :inverse_relationships, :class_name => "Relationship", :foreign_key => "requisite_id"
   has_many :inverse_requisites, :through => :inverse_relationships, :source => :course
-# scope :prerequisites, where(:)
-attr_accessible  :SEC, :INSTRUCTORS, :CRN, :MIN_CRED, :MAX_CRED, :DAYS, :TIME, :RM_CAP, :ENRL, :GLOB_CAP, :LOC_CAP, :AVAIL, :STAT, :FIRECAP, :CAMPUS, :COLLEGE, :LEVEL, :COREQS, :RESTRICTIONS, :PREREQS, :COMMENTS, :SUBJ, :CRS, :TITLE, :COMMENTS
+  # scope :prerequisites, where(:)
+  attr_accessible  :SEC, :INSTRUCTORS, :CRN, :MIN_CRED, :MAX_CRED, :DAYS, :TIME, :RM_CAP, :ENRL, :GLOB_CAP, :LOC_CAP, :AVAIL, :STAT, :FIRECAP, :CAMPUS, :COLLEGE, :LEVEL, :COREQS, :RESTRICTIONS, :PREREQS, :COMMENTS, :SUBJ, :CRS, :TITLE, :COMMENTS
 
 
-def to_node
-  { "id" => self.id,
-    "name" => self.SUBJ + self.CRS + ":" + self.TITLE,
-    "data" => self,
-    "children"   => self.inverse_requisites.map { |c| c.to_node }
-  }
-end
+  def to_node
+    { "id" => self.id,
+      "name" => self.SUBJ + self.CRS + ":" + self.TITLE,
+      "data" => self,
+      "children"   => self.inverse_requisites.map { |c| c.to_node }
+    }
+  end
+
 end
