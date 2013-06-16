25c33688358d6bab3539a5ded7b5dcea81580d52
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-06-15 21:19:18 -0700

    api index responds to a .csv request

--- app/controllers/api/v1/trees_controller.rb
+++ app/controllers/api/v1/trees_controller.rb
@@ -2,48 +2,59 @@ module Api
   module V1
     class TreesController < ApplicationController
       before_filter :restrict_access, :only => [:index, :show]
-
-      respond_to :html, :json, :xml
+      respond_to :html, :json, :xml, :csv
 
 #http://localhost:3000/api/v1/trees?access_token=9f443643180d96624e96b8be4b7af29a&page=3&limit=50
-      def index
-        @courses = Course.paginate(:page => params[:page], :per_page => params[:limit]).order('id DESC')
-
-        @hash =  {:current_page => @courses.current_page,
-                  :per_page => @courses.per_page,
-                  :total_entries => @courses.total_entries,
-                  :entries => @courses.count}
-
-        @response =  {paging: @hash, courses: @courses}.to_json
-        respond_with @response
-      end
-
-      def show
-        @tree = Course.find(params[:id]).to_node
-        respond_with @tree
-      end
-
-      private
-
-      def restrict_access
-        if params[:access_token].present?
-          restrict_access_by_params
-        else
-          restrict_access_by_header
-        end
-      end
-
-      def restrict_access_by_params
-        api_key = ApiKey.find_by_access_token(params[:access_token])
-        head :unauthorized unless api_key
-      end
-
-      def restrict_access_by_header
-        authenticate_or_request_with_http_token do |token, options|
-          ApiKey.exists?(access_token: token)
-        end
-      end
-
+def index
+  @courses = Course.paginate(:page => params[:page], :per_page => params[:limit]).order('id DESC')
+  @hash =  {:current_page => @courses.current_page,
+    :per_page => @courses.per_page,
+    :total_entries => @courses.total_entries,
+    :entries => @courses.count}
+    @response =  {paging: @hash, courses: @courses}.to_json
+    respond_to do |format|
+      format.html {respond_with @response}
+      format.json {respond_with @response}
+      format.xml {respond_with @response}
+      format.csv { render text: Course.to_csv }
+        # send_data Course.to_csv
     end
   end
+
+  def show
+    @tree = Course.find(params[:id]).to_node
+    respond_with @tree
+  end
+
+  def d3test
+    @tree = Course.first.to_node
+  end
+
+  def d3test2
+    @tree = Course.first.to_node
+  end
+
+  private
+
+  def restrict_access
+    if params[:access_token].present?
+      restrict_access_by_params
+    else
+      restrict_access_by_header
+    end
+  end
+
+  def restrict_access_by_params
+    api_key = ApiKey.find_by_access_token(params[:access_token])
+    head :unauthorized unless api_key
+  end
+
+  def restrict_access_by_header
+    authenticate_or_request_with_http_token do |token, options|
+      ApiKey.exists?(access_token: token)
+    end
+  end
+
+end
+end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -16,4 +16,13 @@ class Course < ActiveRecord::Base
     }
   end
 
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |course|
+        csv << course.attributes.values_at(*column_names)
+      end
+    end
+  end
+
 end
